ceb13e8128d6285efa183cf17a527ba1cb1e334e
Jack Barry <jackaldenbarry@gmail.com>
2020-07-27 08:38:03 -0500

    Inch closer to coverage reporting .vue files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [
+    [
+      'babel-plugin-istanbul',
+      {
+        extension: ['.js', '.ts', '.vue'],
+        useInlineSourceMaps: false,
+      },
+    ],
+  ],
 }
